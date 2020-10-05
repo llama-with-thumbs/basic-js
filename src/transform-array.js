2c7947c9f10388b63bd4cdc3bb9ae8d0499cd30f
llama-with-thumbs <vlad.filitovich@gmail.com>
2020-10-05 14:52:40 -0400

    feat: implement hanoi-tower, what-season

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform( arr ) {
+  let transArr = []
+  for( let i = 0; i < arr.length; i++) {
+      switch (arr[i]) {
+          case '--discard-next':
+              i = i + 2
+              break
+          case '--discard-prev':
+              if( i - 1 < 0) break
+              transArr.pop()
+              break
+          case '--double-next':
+              if( i + 1 >= arr.length) break
+              transArr.push(arr[i+1])
+              break
+          case '--double-prev':
+              if( i - 1 < 0) break
+              transArr.push(arr[i-1])
+              break
+          default:
+              transArr.push(arr[i])
+              break
+      }
+  }
+  return transArr
+}
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date == null) return 'Unable to determine the time of year!'
+  if (typeof date.getMonth !== 'function') throw 'Error'
+  if (!(date instanceof Date)) return 'Unable to determine the time of year!'
+
+  if ((new Date()).valueOf() === date.valueOf()) throw 'Error'
+  const arr = [date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()]
+  for (let i = 0; i < arr.length; i++) {
+      if( arr[i] === (arr[i+1] - 1) && (arr[i+1] - 1) === (arr[i+2] - 2)) throw 'Error'
+  }
+
+  const month = parseInt(date.getMonth())
+  return (month == 2 || month == 3 || month == 4) ? 'spring' :
+      (month == 5 || month == 6 || month == 7) ? 'summer' :
+      (month == 8 || month == 9 || month == 10) ? 'fall' :
+      (month == 11 || month == 0 || month == 1) ? 'winter':
+      'Unable to determine the time of year!' 
 };
