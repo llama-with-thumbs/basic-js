5fdc7c988a4c6bccd2b9d03a53d09dc1415eb47c
llama-with-thumbs <vlad.filitovich@gmail.com>
2020-10-06 16:14:40 -0400

    feat: implement chainMaker object with methods

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain : [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      return this.chain.length 
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      this.chain.push(value + '')
+      return this
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      if(Number.isInteger(position) && position > 0 && position - 1 < this.getLength()) {
+          this.chain.splice(position - 1, 1)
+          return this
+      } else { 
+          this.clear()
+          throw 'Noooooo!'
+      }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      this.chain.reverse()
+      return this
+  },
+  clear() {
+      this.chain = []
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      const decor = '( ' + this.chain.join(' )~~( ') + ' )'
+      this.clear()
+      console.log('*** ',decor)
+    return decor
   }
 };
 
