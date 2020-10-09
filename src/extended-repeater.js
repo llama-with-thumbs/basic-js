8071e44f3304e4730beb02c12e0e3d071d7b475c
llama-with-thumbs <vlad.filitovich@gmail.com>
2020-10-09 17:19:16 -0400

    feat: emplement repeter and vigenere encripter

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if(typeof options !== 'object') return str
+  let arr = []
+  let subArr = []
+  let string
+  let subString
+
+  // console.log(options)
+
+  let repeatTimes = 1
+  let separator = '+'
+  let addition = ''
+  let additionRepeatTimes = 1
+  let additionSeparator = '|'
+  if( options.hasOwnProperty('repeatTimes') && options.repeatTimes !== undefined) repeatTimes = options.repeatTimes
+  if( options.hasOwnProperty('separator') && options.separator !== undefined) separator = options.separator + ''
+  if( options.hasOwnProperty('addition') && options.addition !== undefined) addition = options.addition + ''
+  if( options.hasOwnProperty('additionRepeatTimes') && options.additionRepeatTimes !== undefined) additionRepeatTimes = options.additionRepeatTimes
+  if( options.hasOwnProperty('additionSeparator') && options.additionSeparator !== undefined)  additionSeparator = options.additionSeparator + ''
+
+  // console.log(repeatTimes)
+
+  for (let j = 0; j < additionRepeatTimes; j++) {
+    // console.log(j)
+    subArr.push(addition)
+  }
+  subString = subArr.join(additionSeparator)
+  // console.log(subString)
+
+  for (let i = 0; i < repeatTimes; i++) {
+    arr.push(str + subString)
+  }
+  // console.log(arr)
+  string = arr.join(separator)
+  return string
 };
-  
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode) {
+    mode || mode == undefined ? this.mode = true : this.mode = false
+  }
+  encrypt(messege, key) {
+    let encripted = messege.toUpperCase().split('')
+    let keyUpper = key.toUpperCase()
+    let iterator = 0
+    let m
+    let k
+    let c
+    encripted.forEach((element, index, array) => {
+      if (element.match(/[A-Z]/)) {
+        m = element.charCodeAt() - 65
+        k = keyUpper[iterator].charCodeAt() - 65
+        m + k >= 26 ? c = (m + k - 26) + 65 : c = (m + k) + 65
+        array[index] = String.fromCharCode(c)
+        iterator >= (keyUpper.length - 1) ? iterator = 0 : iterator++
+      }
+    });
+    return this.mode ? encripted.join('') : encripted.reverse().join('')
+  }
+  decrypt(messege, key) {
+    let decripted = messege.toUpperCase().split('')
+    let keyUpper = key.toUpperCase()
+    let iterator = 0
+    let m
+    let k
+    let c
+    decripted.forEach((element, index, array) => {
+      if (element.match(/[A-Z]/)) {
+        m = element.charCodeAt() - 65
+        k = keyUpper[iterator].charCodeAt() - 65
+        m - k < 0 ? c = (m - k + 26) + 65 : c = (m - k) + 65
+        // console.log(m - k, String.fromCharCode(m + 65), m + 65, String.fromCharCode(k + 65), k + 65, String.fromCharCode(c), c)
+        array[index] = String.fromCharCode(c)
+        iterator >= (keyUpper.length - 1) ? iterator = 0 : iterator++
+      }
+    });
+    return this.mode ? decripted.join('') : decripted.reverse().join('')
   }
 }
 
